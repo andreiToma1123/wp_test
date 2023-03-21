5e1732826e72dc24c4e363331938a8ed05da9695
Andrei Toma <andrei.toma@gdm.ro>
2023-03-21 11:15:57 +0200

    test commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import { registerBlockType } from '@wordpress/blocks';
+import {registerBlockType} from '@wordpress/blocks';
 import './style.scss';
 import Edit from './edit';
 import Save from './save';
 import metadata from './block.json';
 
-registerBlockType( metadata.name, {
+registerBlockType(metadata.name, {
 	edit: Edit,
 	save: Save,
-} );
+});
